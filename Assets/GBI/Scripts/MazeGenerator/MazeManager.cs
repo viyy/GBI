3333deec70e0e94d9cdac7a94f9aba7e1c2ba378
justgoodwin <saffer1234@mail.ru>
2019-03-17 13:46:25 +0300

    Added meze generator to Maze scene

---
+++ Assets/GBI/Scripts/MazeGenerator/MazeManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+
+	private Maze mazeInstance;
+
+
+	private void Start () {
+		BeginGame();
+	}
+	
+
+
+	private void BeginGame () {
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		
+		mazeInstance.Generate();
+	}
+
+}
