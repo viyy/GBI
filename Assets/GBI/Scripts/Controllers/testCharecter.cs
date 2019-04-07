f6a64fd1d0b1dca89e4b8127dc2115107b1ff994
justgoodwin <saffer1234@mail.ru>
2019-04-07 21:32:13 +0300

    made basic movement and treasure generation

---
+++ Assets/GBI/Scripts/Controllers/testCharecter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCharecter : MonoBehaviour
+{
+    public float speed = 5;
+    private CharacterController _characterController;
+    void Start()
+    {
+        _characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        _characterController.Move(new Vector3(Input.GetAxis("Vertical"),0,-Input.GetAxis("Horizontal")) * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/GBI/Scripts/Controllers/testMission.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMission : MonoBehaviour
+{
+    private GameObject _mazeGameObject;
+    private Maze _mazeScript;
+    public GameObject _treasure;
+    private IntVector2 _tempPos;
+    public bool _generateTreasureFlag;
+    private void Update()
+    {
+        if (_generateTreasureFlag == false)
+        {
+           Generate();         
+        }
+        
+    }
+    public GameObject Generate()
+    {
+        
+        _tempPos = FindMazeOnScene().RandomCoordinates;
+        return GameObject.Instantiate(_treasure,new Vector3(_tempPos.x,1,_tempPos.z), new Quaternion());
+    }
+
+    private Maze FindMazeOnScene()
+    {
+        GameObject _mazeGameObject = GameObject.FindWithTag("Maze");
+        Maze _mazeScript = _mazeGameObject.GetComponentInParent<Maze>() as Maze;
+        _generateTreasureFlag = true;
+        return _mazeScript;
+    }
+}
